9802e670648d0be83c727327d234691be2b1b1ec
Gerardo Baez <g@gerardobaez.com>
2025-09-15 13:04:46 -0400

    Upgrade dependencies, update to Illuminate 9.x, and replace deprecated helpers with full namespace usage for `Str::random`

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -6,7 +6,7 @@ $factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'password' => bcrypt(\Illuminate\Support\Str::random(10)),
+        'remember_token' => \Illuminate\Support\Str::random(10),
     ];
 });
